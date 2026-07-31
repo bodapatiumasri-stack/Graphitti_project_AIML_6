a7496d2b419f7fb3cabbf8242a302ad76b662bc7
mamathadharavath35-a11y <mamathadharavath35@gmail.com>
2026-07-31 23:33:42 +0530

    Update Node.js

--- Node.js
+++ Node.js
@@ -1,2 +1,6 @@
 const path = require('path');
-app.use(express.static(path.join(__dirname, 'public'))); // or 'build' / 'dist'
+const express = require('express');
+const app = express();
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));

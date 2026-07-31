a6e51f41097e0db6a770087d164779a3e2c0dbe2
mamathadharavath35-a11y <mamathadharavath35@gmail.com>
2026-07-31 19:24:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 
 const API_BASE_URL = "https://platter-sandbox-derby.ngrok-free.dev";
 
+function openGraph(targetUrl) {
+    const encodedUrl = encodeURIComponent(targetUrl);
+    const graphIframeUrl = /graph?url=${encodedUrl};
+    window.open(graphIframeUrl, '_blank');
+}
+
 const conversation = document.getElementById('conversation');
 const userInput = document.getElementById('userInput');
 const sendBtn = document.getElementById('sendBtn');

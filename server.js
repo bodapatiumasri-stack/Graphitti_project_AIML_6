0a082281be3553b8fc52d234ef8521c014b3f185
mamathadharavath35-a11y <mamathadharavath35@gmail.com>
2026-07-31 23:41:24 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Serve index.html for the root route
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
